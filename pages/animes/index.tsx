84613b878e19f436b7d7159bc9b8015c66fc4782
Thomas <thomasgleizes34@gmail.com>
2021-11-25 11:36:49 +0100

    add axios (HS)

--- pages/animes/index.tsx
+++ pages/animes/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { GetStaticProps, NextPage } from "next";
+
 import { Anime, CustomResponseData } from "../../types";
+import appAxios from "../../lib/appAxios";
 
 interface Props extends CustomResponseData {
   animes: Array<Anime>;
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const json = await fetch("http://localhost:3001/api/animes").then((res) => res.json());
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const { data } = await appAxios.get<Props>("animes");
 
-  return { props: { ...json }, revalidate: 30 };
+  return { props: data, revalidate: 30 };
 };
 
 const ExploreAnimes: NextPage<Props> = ({ animes }) => {
