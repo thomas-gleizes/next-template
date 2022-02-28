63658f5101e10115abcbe4ec16f3a24312599fb5
Thomas <thomasgleizes34@gmail.com>
2022-02-28 15:25:25 +0100

    fix next page type

--- types/next.d.ts
+++ types/next.d.ts
@@ -1,9 +1,13 @@
 declare module "next/app" {
-  import { NextPage } from "next";
+  import { GetServerSideProps, GetStaticPaths, GetStaticProps, NextPage } from "next";
 
   declare type Page<P = {}> = NextPage<P> & {
-    layout?: Component<{ children: Node; P }>;
+    layout?: Component<{ children: Node } & P>;
   };
+
+  declare type ServerSideProps<T> = GetServerSideProps<T>;
+  declare type StaticProps<T> = GetStaticProps<T>;
+  declare type StaticPaths<T> = GetStaticPaths<T>;
 
   interface AppProps {
     pageProps: any;
